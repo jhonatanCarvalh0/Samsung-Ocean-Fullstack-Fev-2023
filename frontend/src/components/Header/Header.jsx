bdb8ea28e3cfc1ef311f5ac097a419085718ddad
Jhonatan Carvalho <jhonatan.silva.4198@gmail.com>
2023-02-23 08:37:32 -0400

    Criação do componente header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,2 +1,12 @@
-import "<div className="" />
-<Header></Header>.css";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div>
+      <img src="https://oceanbrasil.com/assets/logo.svg" alt="Logo do Ocean" />
+      <a href="#"> Home </a>
+      <a href="#"> Criar </a>
+    </div>
+  );
+}
+export default Header;
